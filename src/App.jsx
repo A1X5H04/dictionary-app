f7bcba933dbc932d843de1899a62594559ea9213
Alish Baig <alishbaig2004@gmail.com>
2023-07-15 16:12:00 +0530

    Reverted to dynamic states

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,23 @@ import Result from "./components/Result";
 import Home from "./components/Home";
 
 function App() {
-  const [searchTerm, setSearchTerm] = React.useState("free");
-  const [dicType, setDicType] = React.useState("classic");
+  const [searchTerm, setSearchTerm] = React.useState("discord");
+  const [dicType, setDicType] = React.useState("urban");
   const [showResult, setShowResult] = React.useState(false);
 
-  return (
-    <>
-      <Result
-        searchTerm={searchTerm}
-        dicType={dicType}
-        setShowResult={setShowResult}
-      />
-    </>
+  return showResult ? (
+    <Result
+      searchTerm={searchTerm}
+      dicType={dicType}
+      setShowResult={setShowResult}
+    />
+  ) : (
+    <Home
+      searchTermState={{ searchTerm, setSearchTerm }}
+      dicTypeState={{ dicType, setDicType }}
+      setShowResult={setShowResult}
+    />
   );
 }
 
 export default App;
-
-{
-  /* <Home
-  searchTermState={{ searchTerm, setSearchTerm }}
-  dicTypeState={{ dicType, setDicType }}
-  setShowResult={setShowResult}
-/> */
-}
